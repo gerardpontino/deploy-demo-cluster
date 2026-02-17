9b1b4823945cb8a970784443b5c77ecaca95d6da
Gerard <46586312+gerardpontino@users.noreply.github.com>
2026-02-17 12:52:09 +0800

    Refactor IAM role to use variable for role name
    Removed hardcoded role name and policies for EKS cluster and nodes.

--- iam.tf
+++ iam.tf
@@ -1,6 +1,5 @@
-# The 1st Role: eksclusterrole (Used for both Cluster and Node per your request)
 resource "aws_iam_role" "eks_cluster_role" {
-  name = "eksclusterrole"
+  name = var.eks_role_name
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -17,28 +16,4 @@ resource "aws_iam_role" "eks_cluster_role" {
       }
     ]
   })
-}
-
-# Policies required for EKS Auto Mode Cluster
-resource "aws_iam_role_policy_attachment" "cluster_auto_mode_policies" {
-  for_each = toset([
-    "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
-    "arn:aws:iam::aws:policy/AmazonEKSComputePolicy",      # Required for Auto Mode
-    "arn:aws:iam::aws:policy/AmazonEKSBlockStoragePolicy", # Required for Auto Mode
-    "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy",# Required for Auto Mode
-    "arn:aws:iam::aws:policy/AmazonEKSNetworkingPolicy"    # Required for Auto Mode
-  ])
-  policy_arn = each.value
-  role       = aws_iam_role.eks_cluster_role.name
-}
-
-# Permissions required for the nodes to join and function
-resource "aws_iam_role_policy_attachment" "node_auto_mode_policies" {
-  for_each = toset([
-    "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
-    "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  ])
-  policy_arn = each.value
-  role       = aws_iam_role.eks_cluster_role.name
 }
